be0078addf9ad277d7294de2b77228fe29046a78
Ahmed Sami <ahmedsami03@protonmail.com>
2023-04-11 07:04:48 +0200

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,10 +23,7 @@ const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppPropsWith
     <>
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
         <meta name="theme-color" content="#000000" />
       </Head>
       <NextSeo {...SEO} />
