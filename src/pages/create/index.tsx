af68ce27f8e888a4af85848b057d73c43a453b38
Ahmed Sami <ahmedsami03@protonmail.com>
2023-04-17 16:04:22 +0200

    .

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,19 +1,18 @@
 import { type NextPageWithAuthAndLayout } from "~/lib/types";
+import { PlusIcon, TagIcon, XIcon } from "lucide-react";
 import AuthDropdown from "~/components/auth-dropdown";
+import { Textarea } from "~/components/ui/textarea";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 import Header from "~/components/header";
 import Footer from "~/components/footer";
 import dynamic from "next/dynamic";
-import { Button } from "~/components/ui/button";
 import {
   Fieldset,
   FieldsetAction,
   FieldsetContent,
   FieldsetFooter,
 } from "~/components/ui/fieldset";
-import { Input } from "~/components/ui/input";
-import { Textarea } from "~/components/ui/textarea";
-import { TagIcon, XIcon } from "lucide-react";
-import { Checkbox } from "~/components/ui/checkbox";
 
 const Provider = dynamic(() => import("~/components/editor/provider"), {
   ssr: false,
@@ -51,17 +50,31 @@ const Create: NextPageWithAuthAndLayout = () => {
     <div className="flex w-full flex-col gap-y-10 sm:max-w-[70ch] md:max-w-[75ch] lg:max-w-[95ch]">
       <Editor placeholder="Start typing here..." />
       <Fieldset>
-        <FieldsetContent className="flex flex-col gap-y-2">
-          <h4 className="text-xl font-[600]">Save Page</h4>
+        <FieldsetContent>
+          <h4 className="mb-1 text-xl font-[600]">Save Page</h4>
           <p className="mb-2 text-sm text-gray-900 dark:text-gray-200">
             In order to save your page, please tell us more about it.
           </p>
-          <Input required placeholder="Title" />
-          <Textarea rows={5} placeholder="Description" />
-          <div className="flex flex-wrap items-center gap-2">
-            {tags.map((tag) => (
-              <Tag key={tag.id} {...tag} />
-            ))}
+          <div className="flex flex-col gap-y-4">
+            <Input placeholder="Title" minLength={1} maxLength={48} required />
+            <Textarea placeholder="Description" rows={5} maxLength={300} />
+            <ul className="flex select-none flex-wrap items-center gap-4">
+              {tags.map((tag) => (
+                <Tag key={tag.id} {...tag} />
+              ))}
+              <li className="inline-flex h-10 rounded-md border border-gray-200 dark:border-gray-800">
+                <button
+                  type="button"
+                  title="Create tag"
+                  className="inline-flex w-full items-center justify-center px-2"
+                >
+                  <PlusIcon
+                    textRendering={"geometricPrecision"}
+                    className="text-gray-700 dark:text-gray-400"
+                  />
+                </button>
+              </li>
+            </ul>
           </div>
         </FieldsetContent>
         <FieldsetFooter>
@@ -76,24 +89,18 @@ const Create: NextPageWithAuthAndLayout = () => {
 
 const Tag = ({ color, id, name }: TagProps) => {
   return (
-    <div className="flex h-8 items-center justify-center rounded border px-2 text-sm font-medium dark:border-gray-800">
-      <Checkbox aria-labelledby={`label-${id}`} />
-      <div className="flex items-center justify-center px-2" id={`label-${id}`}>
-        <TagIcon
-          strokeWidth={2}
-          textRendering={"geometricPrecision"}
-          className="mr-1 h-4 w-4"
-          style={{ color: color }}
-        />
-        {name}
-        <button>
-          <XIcon
-            textRendering={"sharpEdges"}
-            className="relative bottom-1.5 left-2.5 ml-0.5 h-3 w-3 text-gray-600 dark:text-gray-400"
-          />
+    <li className="h-10 flex-1 text-sm font-medium">
+      <input type="checkbox" id={id} value={id} className="peer hidden" />
+      <div className="flex justify-between gap-x-6 rounded border border-gray-200 opacity-60 peer-checked:border-blue-600 peer-checked:opacity-100 dark:border-gray-800">
+        <label htmlFor={id} className="flex flex-1 cursor-pointer items-center gap-x-1.5 py-2 pl-3">
+          <TagIcon textRendering={"geometricPrecision"} className="h-4 w-4" color={color} />
+          <p className="break-keep">{name}</p>
+        </label>
+        <button type="button" title="Delete tag" className="m-1 h-min">
+          <XIcon textRendering={"geometricPrecision"} className="h-4 w-4" />
         </button>
       </div>
-    </div>
+    </li>
   );
 };
 
