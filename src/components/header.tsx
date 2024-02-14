bc932b92870e5e247a445b9c4c80ab18acebcc26
Ahmed Sami <ahmedsami03@protonmail.com>
2024-02-14 17:34:04 +0100

    Add profile page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,15 +5,21 @@ import { type ReactNode } from "react";
 import twcx from "~/utils/twcx";
 import Link from "next/link";
 
-const Header = (props: { children?: ReactNode }) => {
-  const scrolled = useScroll(10);
+export interface HeaderProps {
+  children?: ReactNode;
+  scrollThreshhold?: number;
+}
+
+const Header = ({ children, scrollThreshhold = 25 }: HeaderProps) => {
+  const scrolled = useScroll(scrollThreshhold);
   const onHome = useRouter().asPath === "/";
+  const ignoreScrolled = scrollThreshhold < 1;
 
   return (
     <header
       className={twcx(
         "sticky top-0 z-[101] h-[64px] w-full border-b px-4 py-2 md:py-3",
-        scrolled
+        scrolled || ignoreScrolled
           ? "border-gray-100 bg-white/80 backdrop-blur dark:border-gray-800 dark:bg-black/50"
           : "border-transparent bg-white dark:bg-black"
       )}
@@ -26,7 +32,7 @@ const Header = (props: { children?: ReactNode }) => {
             <Ltx className="h-[22px] w-auto md:h-[28px]" width={56} />
           </Link>
         )}
-        {props.children}
+        {children}
       </nav>
     </header>
   );
--- src/pages/u/[username].tsx
+++ src/pages/u/[username].tsx
@@ -6,6 +6,7 @@ import Footer from "~/components/footer";
 import api from "~/utils/api";
 import ssr from "~/utils/ssr";
 import { Avatar, AvatarImage } from "~/components/ui/avatar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 
 export const getServerSideProps = async (
   context: GetServerSidePropsContext<{ username: string }>
@@ -40,63 +41,25 @@ const ProfilePage: NextPageWithAuthAndLayout<
   }
 
   return (
-    <div className="flex w-full flex-col items-center space-y-12 bg-gray-950 pt-12">
-      <div className="flex w-full max-w-lg flex-col items-center justify-center space-y-1">
-        <Avatar className="mb-2 h-32 w-32">
-          <AvatarImage alt="Profile picture" src={userProfile.image} />
-        </Avatar>
-        <h1 className="text-2xl font-semibold">{userProfile.name}</h1>
-        <p className="text-xs font-light text-gray-400">{userProfile.username}</p>
+    <Tabs defaultValue="overview" className="flex h-screen w-full flex-col">
+      <div className="flex w-full flex-col items-center space-y-12 border-b border-gray-200 bg-gray-950 pt-12 dark:border-gray-800">
+        <div className="flex w-full max-w-lg flex-col items-center justify-center space-y-1">
+          <Avatar className="mb-2 h-32 w-32">
+            <AvatarImage alt="Profile picture" src={userProfile.image} />
+          </Avatar>
+          <h1 className="text-2xl font-semibold">{userProfile.name}</h1>
+          <p className="text-xs font-light text-gray-400">{userProfile.username}</p>
+        </div>
+        <TabsList>
+          <TabsTrigger value="overview">Overview</TabsTrigger>
+          <TabsTrigger value="people">People</TabsTrigger>
+          <TabsTrigger value="stars">Stars</TabsTrigger>
+        </TabsList>
       </div>
-      <div className="w-full border-b border-gray-200 dark:border-gray-800">
-        <ul
-          className="m-auto flex w-full max-w-xl justify-between text-center text-sm font-medium"
-          id="default-tab"
-          data-tabs-toggle="#default-tab-content"
-          role="tablist"
-        >
-          <li role="presentation" className="flex-1">
-            <button
-              className="inline-block px-6 py-3 hover:border-gray-300 hover:text-gray-600 dark:hover:border-gray-700 dark:hover:text-gray-300"
-              id="overview-tab"
-              data-tabs-target="#overview"
-              type="button"
-              role="tab"
-              aria-controls="overview"
-              aria-selected="false"
-            >
-              Overview
-            </button>
-          </li>
-          <li role="presentation" className="flex-1">
-            <button
-              className="inline-block px-6 py-3 hover:border-gray-300 hover:text-gray-600 dark:hover:border-gray-700 dark:hover:text-gray-300"
-              id="people-tab"
-              data-tabs-target="#people"
-              type="button"
-              role="tab"
-              aria-controls="people"
-              aria-selected="false"
-            >
-              People
-            </button>
-          </li>
-          <li role="presentation" className="flex-1">
-            <button
-              className="inline-block px-6 py-3 hover:border-gray-300 hover:text-gray-600 dark:hover:border-gray-700 dark:hover:text-gray-300"
-              id="stars-tab"
-              data-tabs-target="#stars"
-              type="button"
-              role="tab"
-              aria-controls="stars"
-              aria-selected="false"
-            >
-              Stars
-            </button>
-          </li>
-        </ul>
-      </div>
-    </div>
+      <TabsContent value="overview">Overview</TabsContent>
+      <TabsContent value="people">People</TabsContent>
+      <TabsContent value="stars">Stars</TabsContent>
+    </Tabs>
   );
 };
 
@@ -104,7 +67,7 @@ ProfilePage.auth = false;
 ProfilePage.getLayout = (page) => {
   return (
     <>
-      <Header>
+      <Header scrollThreshhold={0}>
         <AuthDropdown />
       </Header>
       <main className="flex min-h-screen flex-col items-center justify-start gap-y-16 pb-24 invert-0 md:gap-y-14 md:pb-32">
