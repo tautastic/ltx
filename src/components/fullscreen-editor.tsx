56cf6da29334a50fd41e14bc08400ab4a86f22af
Ahmed Sami <ahmedsami03@protonmail.com>
2024-02-18 18:51:22 +0100

    Extract editor from create page

---
+++ src/components/fullscreen-editor.tsx
@@ -0,0 +1,189 @@
+import { z } from "zod";
+import { type SubmitHandler, type UseFormReturn } from "react-hook-form";
+import { type Dispatch, type SetStateAction } from "react";
+import { type TagList } from "~/schemas";
+import { useQueryClient } from "@tanstack/react-query";
+import api from "~/utils/api";
+import { useToast } from "~/components/ui/use-toast";
+import { Fieldset, FieldsetContent } from "~/components/ui/fieldset";
+import { BlockEditor } from "~/components/editor/BlockEditor";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
+import { Label } from "~/components/ui/label";
+import { Tag } from "~/components/tag";
+import CreateTagDialog from "~/components/create-tag-dialog";
+import { DialogTrigger } from "~/components/ui/dialog";
+import { PlusIcon } from "lucide-react";
+import { Switch } from "~/components/ui/switch";
+import { Button } from "~/components/ui/button";
+
+export const editorFormSchema = z.object({
+  description: z.string().max(500, {
+    message: "Description must be at most 500 characters.",
+  }),
+  title: z
+    .string()
+    .min(2, {
+      message: "Title must be at least 2 characters.",
+    })
+    .max(64, {
+      message: "Title must be at most 64 characters.",
+    }),
+  isPrivate: z.boolean(),
+});
+
+export type editorFormSchemaType = z.infer<typeof editorFormSchema>;
+
+export interface FullscreenEditorProps {
+  form: UseFormReturn<editorFormSchemaType>;
+  onSubmit: SubmitHandler<editorFormSchemaType>;
+  selectedTagIds: string[];
+  setSelectedTagIds: Dispatch<SetStateAction<string[]>>;
+  tags?: TagList;
+}
+
+export const FullscreenEditor = ({
+  form,
+  onSubmit,
+  selectedTagIds,
+  setSelectedTagIds,
+  tags = [],
+}: FullscreenEditorProps) => {
+  const queryClient = useQueryClient();
+  const deleteTagByIdMutation = api.tags.deleteTagById.useMutation();
+  const { toast } = useToast();
+
+  const handleDeleteTag = async (id: string) => {
+    await deleteTagByIdMutation.mutateAsync(id, {
+      onSuccess: () => {
+        queryClient.invalidateQueries({ stale: true });
+        toast({
+          title: "Deleted tag successfully.",
+        });
+      },
+      onError: () => {
+        toast({
+          title: "🚨 Uh oh! Something went wrong.",
+          description: "Error deleting tag.",
+        });
+      },
+    });
+  };
+
+  return (
+    <Fieldset className="flex w-full flex-1 flex-col items-stretch border-0">
+      <FieldsetContent className="flex min-h-[30ch] w-full items-stretch bg-gray-50 p-0 dark:bg-gray-950 sm:min-h-[40ch]">
+        <BlockEditor
+          className="mx-auto w-full lg:w-10/12 lg:max-w-[125ch]"
+          isHeaderVisible={true}
+        />
+      </FieldsetContent>
+      <Form {...form}>
+        <form
+          onSubmit={(e) => {
+            e.stopPropagation();
+            form.handleSubmit(onSubmit)(e);
+          }}
+        >
+          <Fieldset className="basis-[502px] rounded-none border-x-0 border-b-0">
+            <FieldsetContent className="mx-auto w-full lg:w-8/12 lg:max-w-[110ch]">
+              <h4 className="mb-2 text-xl font-[600]">Save Document</h4>
+              <div className="flex flex-col gap-y-4">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          aria-label="Title"
+                          Size="default"
+                          placeholder="Title"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea aria-label="Description" placeholder="Description" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="space-y-2">
+                  <Label>Tags</Label>
+                  <ul className="flex select-none flex-wrap items-center gap-4">
+                    {tags.map((tag) => (
+                      <Tag
+                        onClick={() => {
+                          setSelectedTagIds([...selectedTagIds, tag.id]);
+                        }}
+                        onDelete={() => handleDeleteTag(tag.id)}
+                        key={tag.id}
+                        {...tag}
+                      />
+                    ))}
+                    <li className="inline-flex h-10 rounded-md border border-gray-200 dark:border-gray-800">
+                      <CreateTagDialog>
+                        <DialogTrigger
+                          type="button"
+                          title="Create tag"
+                          className="inline-flex w-full items-center justify-center px-2"
+                        >
+                          <PlusIcon
+                            textRendering={"geometricPrecision"}
+                            className="text-gray-700 dark:text-gray-400"
+                          />
+                        </DialogTrigger>
+                      </CreateTagDialog>
+                    </li>
+                  </ul>
+                </div>
+                <FormField
+                  control={form.control}
+                  name="isPrivate"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start justify-between space-y-2 rounded-lg border border-gray-200 p-4 dark:border-gray-800">
+                      <div className="space-y-0.5">
+                        <FormLabel className="text-base font-medium">Private</FormLabel>
+                        <FormDescription>
+                          Controls wether or not this Document can be seen by others.
+                        </FormDescription>
+                      </div>
+                      <FormControl>
+                        <Switch checked={field.value} onCheckedChange={field.onChange} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="w-full self-end sm:max-w-32">
+                  Save
+                </Button>
+              </div>
+            </FieldsetContent>
+          </Fieldset>
+        </form>
+      </Form>
+    </Fieldset>
+  );
+};
--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,74 +1,38 @@
 import { type NextPageWithAuthAndLayout } from "~/lib/types";
-import { Textarea } from "~/components/ui/textarea";
-import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
-import { Fieldset, FieldsetContent } from "~/components/ui/fieldset";
-import { BlockEditor } from "~/components/editor/BlockEditor";
-import React, { useState } from "react";
-import { Tag } from "~/components/tag";
+import { useState } from "react";
 import api from "~/utils/api";
 import { useSession } from "next-auth/react";
-import { useQueryClient } from "@tanstack/react-query";
-import CreateTagDialog from "~/components/create-tag-dialog";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "~/components/ui/form";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
 import { useBlockEditor } from "~/lib/hooks/useBlockEditor";
 import { useToast } from "~/components/ui/use-toast";
-import { Label } from "~/components/ui/label";
 import { useRouter } from "next/router";
-import { Switch } from "~/components/ui/switch";
-import { PlusIcon } from "lucide-react";
-import { DialogTrigger } from "~/components/ui/dialog";
-
-const formSchema = z.object({
-  description: z.string().max(500, {
-    message: "Description must be at most 500 characters.",
-  }),
-  title: z
-    .string()
-    .min(2, {
-      message: "Title must be at least 2 characters.",
-    })
-    .max(64, {
-      message: "Title must be at most 64 characters.",
-    }),
-  isPrivate: z.boolean(),
-});
-
-const defaultValues = {
-  description: "",
-  title: "",
-  isPrivate: false,
-};
-
-type formSchemaType = z.infer<typeof formSchema>;
+import {
+  editorFormSchema,
+  type editorFormSchemaType,
+  FullscreenEditor,
+} from "~/components/fullscreen-editor";
 
 const Create: NextPageWithAuthAndLayout = () => {
-  const queryClient = useQueryClient();
   const router = useRouter();
   const { editor } = useBlockEditor();
   const { toast } = useToast();
   const { data: session } = useSession();
-  const deleteTagByIdMutation = api.tags.deleteTagById.useMutation();
   const newPageMutation = api.pages.createNewPage.useMutation();
   const [selectedTagIds, setSelectedTagIds] = useState<string[]>([]);
 
-  const form = useForm<formSchemaType>({
-    resolver: zodResolver(formSchema),
+  const defaultValues = {
+    description: "",
+    title: "",
+    isPrivate: false,
+  };
+
+  const form = useForm<editorFormSchemaType>({
+    resolver: zodResolver(editorFormSchema),
     defaultValues,
   });
 
-  const onSubmit: SubmitHandler<formSchemaType> = async (data) => {
+  const onSubmit: SubmitHandler<editorFormSchemaType> = async (data) => {
     try {
       const res = await newPageMutation.mutateAsync({
         selectedTagIds,
@@ -103,136 +67,20 @@ const Create: NextPageWithAuthAndLayout = () => {
     }
   };
 
-  const handleDeleteTag = async (id: string) => {
-    await deleteTagByIdMutation.mutateAsync(id, {
-      onSuccess: () => {
-        queryClient.invalidateQueries({ stale: true });
-        toast({
-          title: "Deleted tag successfully.",
-        });
-      },
-      onError: () => {
-        toast({
-          title: "🚨 Uh oh! Something went wrong.",
-          description: "Error deleting tag.",
-        });
-      },
-    });
-  };
-
   if (!session || !session.user) {
     return null;
   }
 
-  const { data: tags } = api.tags.getTagListByAuthorId.useQuery(session?.user.id);
+  const { data: tags } = api.tags.getTagListByAuthorId.useQuery(session.user.id);
 
   return (
-    <Fieldset className="flex w-full flex-1 flex-col items-stretch border-0">
-      <FieldsetContent className="flex min-h-[30ch] w-full items-stretch bg-gray-50 p-0 dark:bg-gray-950 sm:min-h-[40ch]">
-        <BlockEditor
-          className="mx-auto w-full lg:w-10/12 lg:max-w-[125ch]"
-          isHeaderVisible={true}
-        />
-      </FieldsetContent>
-      <Form {...form}>
-        <form
-          onSubmit={(e) => {
-            e.stopPropagation();
-            form.handleSubmit(onSubmit)(e);
-          }}
-        >
-          <Fieldset className="basis-[502px] rounded-none border-x-0 border-b-0">
-            <FieldsetContent className="mx-auto w-full lg:w-8/12 lg:max-w-[110ch]">
-              <h4 className="mb-2 text-xl font-[600]">Save Document</h4>
-              <div className="flex flex-col gap-y-4">
-                <FormField
-                  control={form.control}
-                  name="title"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Title</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="text"
-                          aria-label="Title"
-                          Size="default"
-                          placeholder="Title"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Description</FormLabel>
-                      <FormControl>
-                        <Textarea aria-label="Description" placeholder="Description" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div className="space-y-2">
-                  <Label>Tags</Label>
-                  <ul className="flex select-none flex-wrap items-center gap-4">
-                    {tags &&
-                      tags.map((tag) => (
-                        <Tag
-                          onClick={() => {
-                            setSelectedTagIds([...selectedTagIds, tag.id]);
-                          }}
-                          onDelete={() => handleDeleteTag(tag.id)}
-                          key={tag.id}
-                          {...tag}
-                        />
-                      ))}
-                    <li className="inline-flex h-10 rounded-md border border-gray-200 dark:border-gray-800">
-                      <CreateTagDialog>
-                        <DialogTrigger
-                          type="button"
-                          title="Create tag"
-                          className="inline-flex w-full items-center justify-center px-2"
-                        >
-                          <PlusIcon
-                            textRendering={"geometricPrecision"}
-                            className="text-gray-700 dark:text-gray-400"
-                          />
-                        </DialogTrigger>
-                      </CreateTagDialog>
-                    </li>
-                  </ul>
-                </div>
-                <FormField
-                  control={form.control}
-                  name="isPrivate"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-row items-start justify-between space-y-2 rounded-lg border border-gray-200 p-4 dark:border-gray-800">
-                      <div className="space-y-0.5">
-                        <FormLabel className="text-base font-medium">Private</FormLabel>
-                        <FormDescription>
-                          Controls wether or not this Document can be seen by others.
-                        </FormDescription>
-                      </div>
-                      <FormControl>
-                        <Switch checked={field.value} onCheckedChange={field.onChange} />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="w-full self-end sm:max-w-32">
-                  Save
-                </Button>
-              </div>
-            </FieldsetContent>
-          </Fieldset>
-        </form>
-      </Form>
-    </Fieldset>
+    <FullscreenEditor
+      form={form}
+      onSubmit={onSubmit}
+      selectedTagIds={selectedTagIds}
+      setSelectedTagIds={setSelectedTagIds}
+      tags={tags}
+    />
   );
 };
 
