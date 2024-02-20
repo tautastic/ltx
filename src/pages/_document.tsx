7402312007ea8d2b78af61d23522c2834922601b
Ahmed Sami <ahmedsami03@protonmail.com>
2024-02-20 17:39:12 +0100

    Add vercel insights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 class MyDocument extends Document {
   render() {
@@ -23,6 +25,8 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <SpeedInsights />
+          <Analytics />
         </body>
       </Html>
     );
