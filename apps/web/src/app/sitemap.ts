a15e97dc5f8f51ad546862b87d24a8ec877a0be5
Ahmed Sami <ahmedsami03@protonmail.com>
2024-10-07 11:34:23 +0200

    update sitemap

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -15,7 +15,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: "https://ltx.st/u/tautastic@2UYBXW",
+      url: "https://ltx.st/u/tautastic@2HAQMF",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
