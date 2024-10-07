5c0a417b36bcbcdd433220f837c1a1dbebc7c95c
Ahmed Sami <ahmedsami03@protonmail.com>
2024-10-07 12:01:41 +0200

    update sitemap

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -9,16 +9,28 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: "https://ltx.st/auth/signin",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
       url: "https://ltx.st/create",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
+      url: "https://ltx.st/about/privacy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
       url: "https://ltx.st/u/tautastic@2HAQMF",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.5,
+      priority: 0.35,
     },
   ];
 }
