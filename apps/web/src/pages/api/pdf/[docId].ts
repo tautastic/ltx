9ec6c3c69f1ebb93e1a396751a39f5e1de8fab19
Ahmed Sami <ahmedsami03@protonmail.com>
2024-10-08 04:42:07 +0200

    Fix

--- apps/web/src/pages/api/pdf/[docId].ts
+++ apps/web/src/pages/api/pdf/[docId].ts
@@ -11,11 +11,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
+  const pdfMargin = "15mm";
   const formData = new FormData();
   const credentials = btoa(`${env.GOTENBERG_API_BASIC_AUTH_USERNAME}:${env.GOTENBERG_API_BASIC_AUTH_PASSWORD}`);
   formData.append("url", `${env.NEXT_PUBLIC_BASE_URL}/d/${docId}`);
   formData.append("paperWidth", "8.27");
   formData.append("paperHeight", "11.7");
+  formData.append("marginTop", pdfMargin);
+  formData.append("marginRight", pdfMargin);
+  formData.append("marginBottom", pdfMargin);
+  formData.append("marginLeft", pdfMargin);
+  formData.append("printBackground", "true");
 
   const pdfRes = await fetch(`${env.GOTENBERG_API_BASE_URL}/forms/chromium/convert/url`, {
     method: "POST",
