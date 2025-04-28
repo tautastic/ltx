1619d272be77f66a6166f16ced95d27f79a04638
Ahmed Sami <ahmedsami03@protonmail.com>
2025-04-28 18:42:36 +0200

    Try to fix db issues

--- apps/web/src/server/db.ts
+++ apps/web/src/server/db.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 import { env } from "~/env.mjs";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
@@ -9,12 +8,10 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaLibSQL(
-      createClient({
-        url: env.DATABASE_URL,
-        authToken: env.TURSO_AUTH_TOKEN,
-      }),
-    ),
+    adapter: new PrismaLibSQL({
+      url: env.DATABASE_URL,
+      authToken: env.TURSO_AUTH_TOKEN,
+    })
   });
 
 if (env.NODE_ENV !== "production") {
